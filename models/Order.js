ec3a0999a1abec7bd439ed6d122f7a5b449943d8
nathaniel nosa <nathanielnarth@gmail.com>
2024-03-07 11:22:10 +0100

    Models setup

---
+++ models/Order.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const orderSchema = new Schema({
+  userId: {
+    type: String,
+  },
+  products: [
+    {
+      productId: {
+        type: String,
+      },
+      quantity: {
+        type: Number,
+        default: 1
+      }
+    },
+  ],
+  amount: { type: Number },
+  address: { type: Object },
+  status: { type: String, default: "pending" }
+
+}, { timestamps: true })
+
+const orderModel = mongoose.model('Order', orderSchema)
+module.exports = orderModel
---
+++ models/Product.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+  },
+  size: { type: String },
+  color: { type: String },
+  category: { type: Array },
+}, { timestamps: true })
+
+const productModel = mongoose.model('Product', productSchema)
+module.exports = productModel
